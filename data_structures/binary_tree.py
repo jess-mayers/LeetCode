a6bbcd91f31c76542cf193b51f234b6764e8846a
Jess <jbmayers22@gmail.com>
2025-10-06 12:16:58 -0400

    rename static methods

--- data_structures/binary_tree.py
+++ data_structures/binary_tree.py
@@ -12,43 +12,43 @@ class BinaryTree:
     # Traversals
     #############
     @staticmethod
-    def __inorder(node: Node) -> list:
+    def inorder_traversal(node: Node) -> list:
         if node is None:
             return []
         # recurse left
         # root
         # recurse right
-        return BinaryTree.__inorder(node.left) + [node.val] + BinaryTree.__inorder(node.right)
+        return BinaryTree.inorder_traversal(node.left) + [node.val] + BinaryTree.inorder_traversal(node.right)
 
     def inorder(self) -> list:
-        return self.__inorder(self.root)
+        return self.inorder_traversal(self.root)
 
     @staticmethod
-    def __preorder(node: Node) -> list:
+    def preorder_traversal(node: Node) -> list:
         if node is None:
             return []
         # root
         # recurse left
         # recurse right
-        return [node.val] + BinaryTree.__inorder(node.left) + BinaryTree.__inorder(node.right)
+        return [node.val] + BinaryTree.inorder_traversal(node.left) + BinaryTree.inorder_traversal(node.right)
 
     def preorder(self) -> list:
-        return self.__preorder(self.root)
+        return self.preorder_traversal(self.root)
 
     @staticmethod
-    def __postorder(node: Node) -> list:
+    def postorder_traversal(node: Node) -> list:
         if node is None:
             return []
         # recurse left
         # recurse right
         # root
-        return BinaryTree.__inorder(node.left) + BinaryTree.__inorder(node.right) + [node.val]
+        return BinaryTree.inorder_traversal(node.left) + BinaryTree.inorder_traversal(node.right) + [node.val]
 
     def postorder(self) -> list:
-        return self.__postorder(self.root)
+        return self.postorder_traversal(self.root)
 
     @staticmethod
-    def __level_order(node: Node, level: int, level_to_vals: dict) -> list:
+    def level_order_traversal(node: Node, level: int, level_to_vals: dict) -> list:
         if node is None:
             return
         # add list if index does not exist
@@ -56,12 +56,12 @@ class BinaryTree:
             level_to_vals[level] = []
 
         level_to_vals[level].append(node.val)
-        BinaryTree.__level_order(node.left, level + 1, level_to_vals)
-        BinaryTree.__level_order(node.right, level + 1, level_to_vals)
+        BinaryTree.level_order_traversal(node.left, level + 1, level_to_vals)
+        BinaryTree.level_order_traversal(node.right, level + 1, level_to_vals)
 
     def level_order(self) -> list:
         level_to_vals = {}
-        self.__level_order(self.root, 0, level_to_vals)
+        self.level_order_traversal(self.root, 0, level_to_vals)
         level_order = []
         for level in sorted(level_to_vals):
             level_order.extend(level_to_vals[level])
